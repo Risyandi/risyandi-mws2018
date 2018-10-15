465252b2d304e592ad0ac2b2c72d2753fd4d1655
Risyandi <Risyandi.94@gmail.com>
2018-10-15 16:02:14 +0700

    fixing maps and main page

--- public/project-kalkulator/script/addNumbers.js
+++ public/project-kalkulator/script/addNumbers.js
@@ -3,8 +3,6 @@ function kalkulator() {
     let inputPertama = getAngka[0].value; //input pertama
     let inputKedua = getAngka[1].value; //input kedua
     getAngka[2].value = parseInt(inputPertama) + parseInt(inputKedua);
-    let getPesan = document.getElementById('message');
-    getPesan.textContent = "Selesai";
 }
 let tombol = document.querySelector('button');
 tombol.addEventListener('click', kalkulator);
--- public/project-map/script/map.js
+++ public/project-map/script/map.js
@@ -1,19 +1,16 @@
 // initiation map leaflet
-var mymap = L.map('mapid').setView([-6.942645, 107.787223], 13);
+var mymap = L.map('mapid').setView([-6.902353, 107.618778], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
     {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 15,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoicmlzeWFuZGkiLCJhIjoiY2pseGcycWs5MDk4YTNrb3Y3Z2cxeTZ5MCJ9.Bf6nqQP9QQnir2TdQUS2Bw'
-    }).addTo(mymap.panTo(L.latLng(-6.942645, 107.787223)));
+    }).addTo(mymap.panTo(L.latLng(-6.902353, 107.618778)));
 
 // setup data location 
 var dataLocation = [
-    [[-6.942645, 107.787223], "<center><b>Tempat Tinggal</b><br>Jatiroke, Jatinangor</center>"],
-    [[-6.928376, 107.768160], "<center><b>Kampus</b><br>ITB Jatinangor</center>"],
-    [[-6.926666, 107.774631], "<center><b>Kampus</b><br>UNPAD Jatinangor</center>"],
-    [[-6.933924, 107.771434], "<center><b>Mall</b><br>Jatinangor Town Square</center>"],
+    [[-6.902353, 107.618778], "<center><b>Gedung Sate</b><br>Grand Dutch colonial administration building dating to the 1920s, now West Java's government house.</center>"]
 ];
 
 // condition location
