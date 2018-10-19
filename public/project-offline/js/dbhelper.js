62461e8d77dbd2ae1ac6b76a594315c6dc595fc8
Risyandi <Risyandi.94@gmail.com>
2018-10-19 16:52:35 +0700

    update src json

--- public/project-offline/js/dbhelper.js
+++ public/project-offline/js/dbhelper.js
@@ -8,8 +8,9 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    // const port = 8000  | Change this to your server port
+    var source = 'project-offline';
+    return `https://risyandi-mws.firebaseapp.com/${source}/data/restaurants.json`;
   }
 
   /**
@@ -143,14 +144,14 @@ class DBHelper {
    * Restaurant page URL.
    */
   static urlForRestaurant(restaurant) {
-    return (`./restaurant.html?id=${restaurant.id}`);
+    return (`/project-offline/restaurant.html?id=${restaurant.id}`);
   }
 
   /**
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/project-offline/img/${restaurant.photograph}`);
   }
 
   /**
